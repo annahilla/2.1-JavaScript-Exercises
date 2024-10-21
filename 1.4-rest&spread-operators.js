0fcdcaf7e67abe7d91ff0e49f375936259f981ec
Anna <annahilla21@gmail.com>
2024-10-21 13:45:37 +0200

    Fixed spead operator issue

--- 1.4-rest&spread-operators.js
+++ 1.4-rest&spread-operators.js
@@ -1,7 +1,7 @@
 // Nivell 1
 const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
-const array3 = [...array1] + [...array2];
+const array3 = [...array1, ...array2];
 
 const suma = (...args) => {
     let total = 0;
